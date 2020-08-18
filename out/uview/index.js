932d38f47e563cb81174cb09e9d51daffba6f8d5
shipeijun <304684619@qq.com>
2020-08-18 18:04:00 +0800

    initialize commit

---
+++ out/uview/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vscode = require("vscode");
+class CompletionItemProvide {
+    provideCompletionItems(document, position, token, context) {
+        const res = [];
+        for (let i = 0; i < 5; i++) {
+            res.push(new vscode.CompletionItem('aaaaa' + i, vscode.CompletionItemKind.Field));
+        }
+        return res;
+    }
+    resolveCompletionItem(item, token) {
+        return null;
+    }
+}
+exports.default = new CompletionItemProvide();
+//# sourceMappingURL=index.js.map
